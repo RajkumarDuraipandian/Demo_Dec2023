15840ce80f4896c84b17f5941587f9f942d251f4
Rajkumar Duraipandian <rajkumar.duraipandian@gmail.com>
2023-12-14 12:15:17 -0500

    commiton14Dec

--- DemoPurpose.py
+++ DemoPurpose.py
@@ -5,19 +5,19 @@ from diagrams.aws.network import ELB
 from diagrams.aws.network import Route53
 
 with Diagram("AWS-Clustered Web Services", show=False):
-    dns = Route53("dns")
-    lb = ELB("lb")
+    dns = Route53("dns") # domain name system / Amazon route 53
+    lb = ELB("lb") # Elastic load balancing
 
-    with Cluster("Services"):
+    with Cluster("Services"): # Amazon Elastic container service
         svc_group = [ECS("web1"),
                      ECS("web2"),
                      ECS("web3")]
 
-    with Cluster("DB Cluster"):
-        db_primary = RDS("userdb")
-        db_primary - [RDS("userdb ro")]
+    with Cluster("DB Cluster"): # Relational database one for admin write previlage and other one with read only option [Kind of distributed availability group]
+        db_primary = RDS("userdb-write")
+        db_primary - [RDS("userdb-ro")]
 
-    memcached = ElastiCache("memcached")
+    memcached = ElastiCache("memcached") # Amazon ElastiCache
 
     dns >> lb >> svc_group
     svc_group >> db_primary
