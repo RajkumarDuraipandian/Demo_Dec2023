26cd0a0f3727373fcb9b0d0215b9b54dbc09e576
Rajkumar Duraipandian <rajkumar.duraipandian@gmail.com>
2023-12-14 12:30:33 -0500

    2ndcommit14dec

--- DemoPurpose.py
+++ DemoPurpose.py
@@ -8,7 +8,7 @@ with Diagram("AWS-Clustered Web Services", show=False):
     dns = Route53("dns") # domain name system / Amazon route 53
     lb = ELB("lb") # Elastic load balancing
 
-    with Cluster("Services"): # Amazon Elastic container service
+    with Cluster("Services"): # Amazon Elastic container service #
         svc_group = [ECS("web1"),
                      ECS("web2"),
                      ECS("web3")]
